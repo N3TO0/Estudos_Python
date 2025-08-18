1461ecc302a822b1f03700e8601db51dc0c5d7e7
Neto 2 <idervaljose00@gmail.com>
2025-08-18 11:21:11 -0300

    top

--- Exercícios/Operações_e_ Comandos_Basicos/Ex_00.py
+++ Exercícios/Operações_e_ Comandos_Basicos/Ex_00.py
@@ -29,18 +29,95 @@
 
 # # ---------------------------------------------------------
 
-n1=int(input("\nDigite o comprimeiro do primeiro lado:"))
-n2=int(input("\nDigite o comprimeiro do segundo lado:"))
-n3=int(input("\nDigite o comprimeiro do terceiro lado:"))
+# n1=int(input("\nDigite o comprimeiro do primeiro lado:"))
+# n2=int(input("\nDigite o comprimeiro do segundo lado:"))
+# n3=int(input("\nDigite o comprimeiro do terceiro lado:"))
 
-if (n1+n2) > n3 and (n3 +n2) > n1 and (n1 +n3) > n2:
-    print("\n\nÉ prossivel formar um triangulo\n")
-else :
-    print("\n\nNão é possivel formar um triangulo\n")
+# if (n1+n2) > n3 and (n3 +n2) > n1 and (n1 +n3) > n2:
+#     print("\n\nÉ prossivel formar um triangulo\n")
+# else :
+#     print("\n\nNão é possivel formar um triangulo\n")
 
-#---------------------------------------------------------
+# #---------------------------------------------------------
+# while 1:
+    
+#     lado1= input("digite o primeiro lado do triangulo")
+#     lado2= input("digite o segundo lado do triangulo")
+#     lado3= input("digite o terceiro lado do triangulo")
+
+#     if lado1 
+
+# ---------------------------------------------------------
+
+# base=int(input("digire a base do triangulo: "))
+# altura=int(input("digite a altura do triangulo: "))
+
+# print(f"A area do triangulo é: {(base*altura)/2}")
+
+#----------------------------------------------------------
+
+# segundos=int(input("Digite os segundo: "))
+# minutos=int(0)
+# horas=int(0)
+
+# while segundos >= 60:
+#     segundos= segundos - 60
+#     minutos = minutos + 1
+
+# while minutos >= 60:
+#     minutos = minutos - 60
+#     horas = horas + 1
+
+# print(f"O calculo de horas, munituos, segundo é: {horas}:{minutos}:{segundos}")
+
+# --------------------------------------------------------
+
+# segundos = int(input("\nDigite os segundos: "))
+# minutos = int(0)
+# horas = int(0)
+ 
+# horas = segundos // 3600
+# segundos = segundos % 3600
+
+# minutos = segundos // 60
+# segundos = segundos % 60
+
+# print(f"\n\nhoras: {horas} \nMinutos: {minutos} \nSegundos: {segundos} \n\n")
+
+# ---------------------------------------------------------
+
+# CAPITAL = int(input("\nDigite quanto de capital deseja investir: "))
+# TAXA_DE_JUROS = float(input("\nDigite qual a porcentagem do juros por dia: "))
+# TEMPO_DE_JUROS = int(input("\nDigite quantos dias deseja receber de volta: "))
+
+# print(f"\n\nO valor que irá receber é: R$ {((( TAXA_DE_JUROS / CAPITAL ) * 100 ) * TEMPO_DE_JUROS ) + CAPITAL }\n\n")
+
+# ---------------------------------------------------------
+
+# ENTRADA=int(input("\nDigite um numero para saber se é par ou impár e se esse numero é maior ou menor que 15\n\nNumero: "))
 
 
+# if ENTRADA < 15 :
+#     print(f"\n'{ENTRADA}' É menor que 15!!")
+# elif ENTRADA == 15:
+#     print(f"'{ENTRADA}' É igual a 15")
+# else:
+#     print(f"\n'{ENTRADA}' é maior que 15!!")
 
+# if ENTRADA % 2 == 0:
+#     print(f"\n\n'{ENTRADA}' é um numero par\n")
+# else:
+#     print(f"\n\n'{ENTRADA}' é um numerO impár\n")
 
+# ---------------------------------------------------------
 
+# LAD1=int("Digite o comprimento do primeiro lado: ")
+# LAD2=int("Digite o comprimento do segundo lado: ")
+# LAD3=int("Digite o comprimento do terceiro lado: ")
+
+# if (LAD1 == LAD2 or LAD2 == LAD3 or LAD1 == LAD3) and ( (LAD1 + LAD2) > LAD3 or (LAD3+ LAD1) > LAD2 or (LAD3 + LAD2) > LAD1 ):
+#     print("Esse é um triangulo isóceles!")
+# else:
+#     print("Não é um triangulo isóceles!!")
+
+# # ----------------------------------------------------------
